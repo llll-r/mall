11b3488ec5bbbaf0ed78607c3b8ec56682e3879e
LvRui <llvrui@163.com>
2020-07-23 17:08:36 +0800

    ddd

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+const Category = () => import("../views/category/Category.vue")
+const Profile = () => import("../views/profile/Profile.vue")
+const Home = () => import("../views/home/Home.vue")
+const Cart = ()=>import( "../views/cart/Cart.vue")
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '',
+    redirect:"/home"
+    },
+    {
+    path: '/home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    },
+    {
+      path: '/cart',
+      component: Cart
+    },
+    {
+      path: '/profile',
+      component:Profile
+    },
+    {
+      path: '/category',
+      component: Category
+    }
 ]
 
 const router = new VueRouter({
---
+++ src/views/home/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+<nav-bar class="home-nav">
+  <div slot="center" class="center">购物街</div>
+</nav-bar>
+   
+  </div>
+</template>
+home-nav
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from "../../components/common/navbar/NavBar"
+import {getHomeMultidata} from "../../network/home"
+export default {
+  name: 'Home',
+  data(){
+    return{
+      result:null,
+      dkeyword:[],
+      banner:[],
+      keywords:[],
+      recommend:[],
+    }
+  },
+  components:{
+    NavBar
+  },
+  created(){
+    // let _this = this
+    getHomeMultidata().then(res=>{
+      console.log(res)
+      this.banner = res.data.banner
+      this.dkeyword = res.dkeyword
+      this.keywords = res.keywords
+      this.recommend = res.recommend
+      // this.result = res
+    })
+  }
+ 
+}
+</script>
+<style  scoped>
+.home-nav{
+  background-color: rgb(243, 149, 212);
+}
+.center{
+ 
+  color: white;
+}
+</style>
