f3a338f8977b38be8a140d74d5e3e5614e1f2a94
LvRui <llvrui@163.com>
2020-07-24 18:25:04 +0800

    rrr

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="goodsList">
+<!-- <h2>我是Goods List</h2> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name:"GoodsList",
+   props:{
+       goods:{
+           type:Array,
+           default(){
+               return []
+           }
+       }
+   }
+}
+</script>
+
+<style>
+
+</style>
